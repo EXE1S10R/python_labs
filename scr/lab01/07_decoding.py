7c39fbd2525b060c298b40b12bf3247c7e1a8d97
Maksim Pavlov <rabotpcht@gmail.com>
2026-09-10 11:28:25 +0300

    Исправление 7 файла, добавление скрина и создание пикми

--- scr/lab01/07_decoding.py
+++ scr/lab01/07_decoding.py
@@ -1,9 +1,6 @@
 string = input()
 first_index = min([i for i in range(len(string)) if string[i] == string[i].upper() and string[i].isalpha()])
-print(first_index)
-second_index = min([i for i in range(1, len(
-    string)) if string[i-1].isdigit() and string[i].isalpha() and i > first_index])
-print(second_index)
+second_index = min([i for i in range(1, len(string)) if string[i-1].isdigit() and string[i].isalpha() and i > first_index])
 step = second_index - first_index
 
 print(''.join([string[i] for i in range(first_index, len(string), step)]).split('.')[0])
