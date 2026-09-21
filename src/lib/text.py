13ed009cc814c7a752c15facf8124e6a9414e8b8
Maksim Pavlov <rabotpcht@gmail.com>
2026-09-21 14:15:22 +0300

    Добавил функции в либ, прописал тесты

--- src/lib/text.py
+++ src/lib/text.py
@@ -16,9 +16,56 @@ def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
     if casefold:
         new_text = new_text.casefold()
     if yo2e:
-        new_text.replace('ё', 'е').replace('Ё', 'Е')
+        new_text = new_text.replace('ё', 'е').replace('Ё', 'Е')
     new_text = ' '.join(new_text.split())
     return new_text
 
 def tokenize(text: str) -> list[str]:
     return [match.group() for match in re.finditer(r'\w+(-\w+)*', text)]
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    '''Считает количество повторений токенов
+    
+    Args:
+        tokens: Список слов (токенов)
+    
+    Returns:
+        freq: Словарь: [слово], [количество повторений]
+    '''
+    freq = {}
+    for el in set(tokens):
+        freq[el] = tokens.count(el)
+    return freq
+
+def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    '''Выводит топ n слов по частоте обращения
+    
+    Args:
+        freq: Словарь где указаны слова и количество их повторений
+        n (=5): Количество элементов которые нужно указать в топе
+
+    Returns:  Список где находятся кортежи (слово, количество повторений)
+    '''
+    return sorted(freq.items(), key=lambda x: (-x[1], x[0]))[:n]
+
+
+print('Тест кейсы:')
+
+print(fr'''
+normalize
+
+"ПрИвЕт\nМИр\t" -> {normalize("ПрИвЕт\nМИр\t")}
+"ёжик, Ёлка" -> {normalize("ёжик, Ёлка")}
+"Hello\r\nWorld" -> {normalize("Hello\r\nWorld")}
+"  двойные   пробелы  " -> {normalize("  двойные   пробелы  ")}
+''')
+
+print(fr'''
+tokenize
+
+"привет мир" -> {tokenize("привет мир")}
+"hello,world!!!" -> {tokenize("hello,world!!!")}
+"по-настоящему круто" -> {tokenize("по-настоящему круто")}
+"2025 год" -> {tokenize("2025 год")}
+"emoji 😀 не слово" -> {tokenize("emoji 😀 не слово")}
+''')
